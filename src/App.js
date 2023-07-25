178ba5beef17a4c8afd3ed74262cf3a4ff0f2b3c
jswan <jswannieman@gmail.com>
2023-07-25 21:42:46 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React, { useState } from "react";
+import ClassComponentWithContext from "./ClassComponentWithContext";
+import FunctionComponentWithContext from "./FunctionComponentWithContext";
 
-function App() {
-  return <div className="App"></div>;
+export const ThemeContext = React.createContext();
+
+export default function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+    setCounter((prevCounter) => prevCounter + 1);
+    if (!darkTheme) {
+      document.body.style.backgroundColor = "navy";
+      document.getElementById("butty").style.backgroundColor = "lightblue";
+      document.getElementById("butty").style.color = "black";
+    } else {
+      document.body.style.backgroundColor = "lightseagreen";
+      document.getElementById("butty").style.backgroundColor = "lightgreen";
+      document.getElementById("butty").style.color = "black";
+    }
+  }
+
+  return (
+    <div className="App">
+      <ThemeContext.Provider value={darkTheme}>
+        <div style={{ width: "40%", marginLeft: "30%", marginRight: "30%" }}>
+          <button id="butty" onClick={toggleTheme}>
+            Toggle Theme
+          </button>
+          <p className="cntr">Toggle Counter = {counter}</p>
+          <FunctionComponentWithContext />
+          <ClassComponentWithContext />
+        </div>
+      </ThemeContext.Provider>
+    </div>
+  );
 }
-
-export default App;
